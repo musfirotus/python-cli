e73f02da2a44ceccbc2b48c8a869146f173e572c
musfirotus <musfirotus@gmail.com>
2020-09-09 20:35:00 +0700

    finished

--- fetchdata/a_post.py
+++ fetchdata/a_post.py
@@ -1,31 +1,23 @@
-import asyncio
 import aiohttp
+import asyncio
 import time
+import json
+from Fetch import Fetcher as fetcher
 
 urls = 'https://jsonplaceholder.typicode.com/posts'
 
-async def fetch(urls):
-    async with aiohttp.ClientSession as session:
-        async with session.get(urls) as resp:
-            print(resp.status)
-    
-async def get_title(urls):
+async def fetch_url():
     start = time.time()
     print(f"started at {time.strftime('%X')}")
     
-    list_tasks = []
-    for url in urls:
-        task = asyncio.create_task(fetch(url))
-        list_tasks.append(task)
-        
-    for task in list_tasks:
-        await task
+    html = await fetcher.get(urls)
+    loads = json.loads(html)
+    print('\n'.join(list(map(lambda loads: f"title: {loads['title']}", loads))))
     
     end = time.time()
     print(f"Finished at {time.strftime('%X')}")
     print(f"Total time: {round(int(end - start))}")
-    
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     loop = asyncio.get_event_loop()
-    # loop.run_until_complete(run_task(list_url))
-    loop.run_until_complete(get_title(urls))
+    loop.run_until_complete(fetch_url())
